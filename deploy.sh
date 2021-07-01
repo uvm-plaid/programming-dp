ef6d3e08d3072efd11f4a50090ea8b6a3717a793
Chike Abuah <abuahchu@gmail.com>
2021-07-01 10:30:15 -0400

    update homepage

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 jupyter-book build .
 cp static/index.html _build/html/
-cp static/book-logo.png _build/html/ 
+cp static/book-logo.png _build/html/
+cp book.pdf _build/html/ 
 ghp-import -n -p -f _build/html
