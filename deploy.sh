f1c3472319eaed2374dea82ff7f82ad9be1ba569
Joe Near <jnear@uvm.edu>
2021-07-07 07:17:47 -0400

    Fix Binder links and make CNAME permanent

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,6 @@ jupyter-book build .
 jupyter-book build --builder pdflatex .
 cp static/index.html _build/html/
 cp static/book-logo.png _build/html/
+cp static/CNAME _build/html/
 cp _build/latex/book.pdf _build/html/ 2>/dev/null || :
 ghp-import -n -p -f _build/html
